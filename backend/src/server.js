ba38a737704546a6546a62fcaf356d9bf64bf372
Andika Sentosa Putra <noname18@student.ubc.ca>
2026-01-06 21:07:38 -0800

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express"; // need to add type module in package.json
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path"; // for deployment
 
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
@@ -11,15 +12,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // for deployment
 
 // middleware: function that runs between request and response
 app.use(express.json()); // to parse JSON bodies: req.body; must be before routes
 
 // app.use(cors()); // allow every requests from every single url
 // this should be before ratelimiter
-app.use(cors({
-  origin: "http://localhost:5173"
-})); // originally this is not allowed e.g. we are coming from frontend localhost:3000 then send a request to API backend at http://api.example.com
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: "http://localhost:5173"
+  })); // originally this is not allowed e.g. we are coming from frontend localhost:3000 then send a request to API backend at http://api.example.com
+}
+  
 
 app.use(rateLimiter);
 
@@ -31,6 +36,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/../frontend/dist"))); // for deployment
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../frontend", "dist", "index.html"));
+  })
+}
+
 
 // connectDB(); // better to guarantee connect db first before listening
 connectDB().then(() => {
